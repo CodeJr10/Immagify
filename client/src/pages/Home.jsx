c3e0b9448162bc9698385062092e606a22241675
CodeJr10 <aadityamalap666@gmail.com>
2025-04-06 22:03:32 +0530

    Implement Steps section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
 import React from "react";
+import Steps from "../components/Steps";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Steps />
     </div>
   );
 };
