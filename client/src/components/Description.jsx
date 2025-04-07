4b97f22676184a6e7141bc5b0d78b14d30c6da7d
CodeJr10 <aadityamalap666@gmail.com>
2025-04-07 20:51:58 +0530

    Fixed Styling for description section

--- client/src/components/Description.jsx
+++ client/src/components/Description.jsx
@@ -9,21 +9,23 @@ const Description = () => {
       </h1>
       <p className="text-gray-500 mb-8">Turn your imagination into visuals</p>
 
-      <div>
+      <div className="flex flex-col gap-5 md:gap-14 md:flex-row items-center">
         <img
           src={assets.sample_img_1}
           alt=""
           className="w-80 xl:w-96 rounded-lg"
         />
         <div>
-          <h2>Introducing the AI-Powered Text to Image Generator</h2>
-          <p>
+          <h2 className="text-3xl font-medium max w-lg mb-4">
+            Introducing the AI-Powered Text to Image Generator
+          </h2>
+          <p className="text-gray-600 mb-4">
             Easily Bring your ideas to life with our free AI image generator.
             Whether you need stunning visuals or unique imagery, our tool
             transforms your text into eye-catching images with just a few
             clicks. Imagine it, describe it, and watch it come to life instantly
           </p>
-          <p>
+          <p className="text-gray-600">
             Simply type in a text prompt, our cutting-edge AI will generate
             high-quality images in seconds.
           </p>
