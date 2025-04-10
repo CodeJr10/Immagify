ae5892ddd85a359ae5a64488b3a1d76bce411078
CodeJr10 <aadityamalap666@gmail.com>
2025-04-10 22:55:53 +0530

    Login and SignUp component added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,20 @@
+import React, { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 
+import { AppContext } from "./context/AppContext";
 import BuyCredit from "./pages/BuyCredit";
 import Footer from "./components/footer";
 import Home from "./pages/Home";
+import Login from "./components/Login";
 import Navbar from "./components/Navbar";
-import React from "react";
 import Result from "./pages/Result";
 
 const App = () => {
+  const { showLogin } = useContext(AppContext);
   return (
     <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b frm-teal-50 to-orange-50">
       <Navbar />
+      {showLogin && <Login />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/result" element={<Result />} />
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,8 +5,9 @@ import { AppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
 
 const Navbar = () => {
-  const { user } = useContext(AppContext);
+  const { user, setShowLogin } = useContext(AppContext);
   const navigate = useNavigate();
+
   return (
     <div className="flex items-center justify-between py-4">
       <Link to="/">
@@ -45,7 +46,10 @@ const Navbar = () => {
             <p className="cursor-pointer" onClick={() => navigate("/buy")}>
               Pricing
             </p>
-            <button className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full">
+            <button
+              className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full"
+              onClick={() => setShowLogin(true)}
+            >
               Login
             </button>
           </div>
