c32f65c057c8f4a97f93c26f1024a36c209831b2
CodeJr10 <aadityamalap666@gmail.com>
2025-04-11 20:55:07 +0530

    Implemented handler function on generate button

--- client/src/components/GenerateBtn.jsx
+++ client/src/components/GenerateBtn.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { AppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
 import { motion } from "motion/react";
+import { useNavigate } from "react-router-dom";
 
 const GenerateBtn = () => {
+  const { user, setShowLogin } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  const onClickHandler = () => {
+    if (user) {
+      navigate("/result");
+    } else {
+      setShowLogin(true);
+    }
+  };
+
   return (
     <motion.div
       className="pb-16 text-center"
@@ -14,7 +28,10 @@ const GenerateBtn = () => {
       <h1 className="text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold text-neutral-800 py-6 md:py-16">
         See the magic. Try now
       </h1>
-      <button className="inline-flex items-center gap-2 px-12 py-3 rounded-full bg-black text-white m-auto hover:Scale-105 transition-all duration-500">
+      <button
+        className="inline-flex items-center gap-2 px-12 py-3 rounded-full bg-black text-white m-auto hover:Scale-105 transition-all duration-500"
+        onClick={onClickHandler}
+      >
         Generate Images
       </button>
       <img src={assets.star_group} alt="" className="h-6" />
