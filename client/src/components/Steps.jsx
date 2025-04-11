b6376d634d9b807a822e62b93db8b96a4e16bf8d
CodeJr10 <aadityamalap666@gmail.com>
2025-04-11 20:46:48 +0530

    Added animation in the steps section

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -4,7 +4,13 @@ import { stepsData } from "../assets/assets";
 
 const Steps = () => {
   return (
-    <motion.div className="flex flex-col items-center justify-center my-32">
+    <motion.div
+      className="flex flex-col items-center justify-center my-32"
+      initial={{ opacity: 0.2, y: 100 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+    >
       <h1 className="text-3xl sm:text-4xl font-semibold mb-2">How it works</h1>
 
       <p className="text-lg text-gray-600 mb-8">
