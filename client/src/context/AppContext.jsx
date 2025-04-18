f98124811ffc6db845d649edcb74b36333e26fd0
CodeJr10 <aadityamalap666@gmail.com>
2025-04-18 19:01:03 +0530

    Image generator function added

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 
 import axios from "axios";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
@@ -13,6 +14,8 @@ const AppContextProvider = (props) => {
 
   const [credit, setCredit] = useState(false);
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const navigate = useNavigate();
   const loadCreditsData = async () => {
     try {
       const { data } = await axios.get(backendUrl + "/api/user/credits", {
@@ -22,6 +25,30 @@ const AppContextProvider = (props) => {
       if (data.success) {
         setCredit(data.credits);
         setUser(data.user);
+      }
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
+  const generateImage = async (prompt) => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/image/generate-image",
+        { prompt },
+        { headers: { token } }
+      );
+
+      if (data.success) {
+        loadCreditsData();
+        return data.resultImage;
+      } else {
+        toast.error(data.message);
+        loadCreditsData();
+        if (data.creditBalance === 0) {
+          navigate("/buy");
+        }
       }
     } catch (error) {
       console.log(error.message);
@@ -53,6 +80,7 @@ const AppContextProvider = (props) => {
     setCredit,
     loadCreditsData,
     logout,
+    generateImage,
   };
 
   return (
