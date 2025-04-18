bae800eb00f0594c303178f384ad691a90e2e529
CodeJr10 <aadityamalap666@gmail.com>
2025-04-18 19:37:34 +0530

    Implement initialzed rayzorpay function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import razorpay from "razorpay";
 import userModel from "../models/userModel.js";
 
 const registerUser = async (req, res) => {
@@ -68,6 +69,60 @@ const userCredits = async (req, res) => {
     });
   } catch (error) {
     console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+const razorpayInstance = new razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_SECRET,
+});
+
+const paymentRazorpay = async (req, res) => {
+  try {
+    const { userId, planId } = req.body;
+    const userData = await userModel.findById(userId);
+
+    if (!userId || planId) {
+      return res.json({ success: false, message: "Missing Details" });
+    }
+
+    let credits, plan, amount, date;
+
+    const transactionData = {
+      userId,
+      plan,
+      credits,
+      plan,
+      amount,
+      date,
+    };
+
+    switch (planId) {
+      case "Basic":
+        plan: "Basic";
+        credits: 100;
+        amount: 10;
+        break;
+
+      case "Advanced":
+        plan: "Advanced";
+        credits: 500;
+        amount: 50;
+        break;
+
+      case "Business":
+        plan: "Business";
+        credits: 5000;
+        amount: 250;
+        break;
+
+      default:
+        return res.json({ success: false, message: "Invalid Plan" });
+    }
+
+    date = Date.now();
+  } catch (error) {
     res.json({ success: false, message: error.message });
   }
 };
