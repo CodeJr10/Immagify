80e6b13fc3e36f313b1d5902640eee5e1e1d573d
CodeJr10 <aadityamalap666@gmail.com>
2025-04-18 19:08:43 +0530

    Fixed missing details error while generating image

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -4,8 +4,8 @@ import userModel from "../models/userModel.js";
 
 export const generateImage = async (req, res) => {
   try {
-    const { userId, prompt } = req.body;
-
+    const prompt = req.body.prompt;
+    const userId = req.user.id;
     const user = await userModel.findById(userId);
 
     if (!user || !prompt) {
